90c000e08620e526f2a44c905f971d45f9b7706e
HJ <tugratias@gmail.com>
2017-06-15 11:45:56 +0900

    login view, template, urls생성 및 연결

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -41,7 +41,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-STATIC_DIR = os.path.join(BASE_DIR, 'staitc')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
--- django_app/post/admin.py
+++ django_app/post/admin.py
@@ -5,5 +5,3 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-
-admin.site.register(Post)
