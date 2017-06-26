491742d478283b8ed765a495c7b1a7b207cfc49f
HJ <tugratias@gmail.com>
2017-06-26 15:13:09 +0900

    gir

--- django_app/member/models.py
+++ django_app/member/models.py
@@ -1,5 +1,53 @@
-from django.contrib.auth.models import AbstractUser
+import re
+
+import requests
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser, UserManager as DefaultUserManager
+from django.core.files import File
+from django.core.files.temp import NamedTemporaryFile
 from django.db import models
+
+from utils.fields import CustomImageField
+
+
+class UserManager(DefaultUserManager):
+    def get_or_create_facebook_user(self, user_info):
+        username = '{}_{}_{}'.format(
+            self.model.User.USER_TYPE_FACEBOOK,
+            settings.FACEBOOK_APP_ID,
+            user_info['id']
+        )
+        user, user_created = self.get_or_create(
+            username=username,
+            user_type=self.model.USER_TYPE_FACEBOOK,
+            defaults={
+                'last_name': user_info.get('last_name', ''),
+                'first_name': user_info.get('first_name', ''),
+                'email': user_info.get('email', ''),
+            }
+        )
+        # 유저가 새로 생성되었을 때만 프로필 이미지를 받아옴
+        if user_created:
+            # https://scontent.xx.fbcdn.net/v/t1.0-1/p50x50/17190366_634177590104859_7473999312831140266_n.jpg?oh=4c68d08b4066747c1c7c7bcb32670b6a&oe=59DA648E
+            # 프로필 이미지 URL
+            url_picture = user_info['picture']['data']['url']
+            # 파일확장자를 가져와서, 유저고유의 파일명을 만들어줌
+            p = re.compile(r'.*\.([^?]+)')
+            file_ext = re.search(p, url_picture).group(1)
+            file_name = '{}.{}'.format(
+                user.pk,
+                file_ext
+            )
+            # 이미지파일을 임시저장할 파일객체
+            temp_file = NamedTemporaryFile(delete=False)
+            # 프로필 이미지 URL에 대한 get요청 (이미지 다운로드)
+            response = requests.get(url_picture)
+            # 요청 결과를 temp_file에 기록
+            temp_file.write(response.content)
+            # ImageField의 save()메서드를 호출해서 해당 임시파일객체를 주어진 이름의 파일로 저장
+            # 저장하는 파일명은 위에서 만든 <유저pk.주어진파일확장자> 를 사용
+            user.img_profile.save(file_name, File(temp_file))
+        return user
 
 
 class User(AbstractUser):
@@ -24,12 +72,29 @@ class User(AbstractUser):
         나의 friends는 고성현 1명이다
     """
     # 이 User모델을 AUTH_USER_MODEL로 사용하도록 settings.py에 설정
+    USER_TYPE_DJANGO = 'd'
+    USER_TYPE_FACEBOOK = 'f'
+    USER_TYPE_CHOICES = (
+        (USER_TYPE_DJANGO, 'Django'),
+        (USER_TYPE_FACEBOOK, 'Facebook'),
+    )
+    # 유저타입. 기본은 Django이며, 페이스북 로그인시 USER_TYPE_FACEBOOK값을 갖도록 함
+    user_type = models.CharField(max_length=1, choices=USER_TYPE_CHOICES, default=USER_TYPE_DJANGO)
     nickname = models.CharField(max_length=24, null=True, unique=True)
+    img_profile = CustomImageField(
+        upload_to='user',
+        blank=True,
+        # default_static_image='images/profile.png',
+    )
     relations = models.ManyToManyField(
         'self',
         through='Relation',
         symmetrical=False,
     )
+
+    # 위에서 만든 CustomUserManager를 objects속성으로 사용
+    # User.objects.create_facebook_user()메서드 실행 가능
+    objects = UserManager()
 
     def __str__(self):
         return self.nickname or self.username
@@ -67,6 +132,11 @@ class User(AbstractUser):
 
     def is_follow(self, user):
         # 해당 user를 내가 follow하고 있는지 bool여부를 반환
+        # ModelManager.exists()
+        return self.follow_relations.filter(to_user=user).exists()
+
+    def is_follower(self, user):
+        # 해당 user가 나를 follow하고 있는지 bool여부를 반환
         return self.follower_relations.filter(from_user=user).exists()
 
     def follow_toggle(self, user):
@@ -79,12 +149,15 @@ class User(AbstractUser):
 
     @property
     def following(self):
+        # https://docs.djangoproject.com/en/1.11/ref/models/querysets/#in
         relations = self.follow_relations.all()
-        return User.objects.filter(pk__in=relations.values('pk'))
+        return User.objects.filter(pk__in=relations.values('to_user'))
+
     @property
     def followers(self):
         relations = self.follower_relations.all()
-        return User.objects.filter(pk__in=relations.values('pk'))
+        return User.objects.filter(pk__in=relations.values('from_user'))
+
 
 class Relation(models.Model):
     from_user = models.ForeignKey(User, related_name='follow_relations')
